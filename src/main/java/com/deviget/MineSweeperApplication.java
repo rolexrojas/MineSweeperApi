0b7872ec085b3e95744fcdb5174cff90231828dd
Roberto <Robertorojas@outlook.com>
2021-10-01 13:28:26 -0400

    Clean Set Up Project, First Commit

---
+++ src/main/java/com/deviget/MineSweeperApplication.java
@@ -0,0 +1,13 @@
+package com.deviget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineSweeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineSweeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deviget/CustomMineSweeper/CustomMineSweeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deviget.CustomMineSweeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomMineSweeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
